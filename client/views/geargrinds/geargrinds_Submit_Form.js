5c161fc99c9812f9365bca3ec830a26721b29d5d
Cody <meanderingmonarch@gmail.com>
2015-07-10 17:23:44 -0500

    updates 34

---
+++ client/views/geargrinds/geargrinds_Submit_Form.js
@@ -0,0 +1,18 @@
+Template.geargrindsSubmitForm.events({
+  'submit form': function(e){
+    e.preventDefault();
+    
+    var grind = {
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    Meteor.call('grindInsert', grind, function(error, result){
+      
+      if(error){
+        return alert(error.reason);
+      }
+      
+      Router.go('/');
+    });
+  }
+});
